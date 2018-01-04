5c0acd8734f675854c0ca7cb7c4d422c541d459f
zhzhgang <zhang_zhonggang@163.com>
2018-01-04 16:51:55 +0800

    发布 dubbo 服务

---
+++ mall-manager/mall-manager-interface/src/main/java/com/zhzhgang/mall/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.zhzhgang.mall.service;
+
+import com.zhzhgang.mall.pojo.MallItem;
+
+/**
+ * @author zhangzhonggang
+ * @create 2018-01-04 15:50
+ */
+public interface ItemService {
+
+    MallItem getItemById(long itemId);
+
+}
