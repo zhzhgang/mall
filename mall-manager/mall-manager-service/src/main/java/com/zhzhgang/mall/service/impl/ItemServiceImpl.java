c0cf29496c5525bd66db7289f88c44a7cdb65f1a
zhzhgang <zhang_zhonggang@163.com>
2018-01-05 18:32:41 +0800

    商品列表查询

--- mall-manager/mall-manager-service/src/main/java/com/zhzhgang/mall/service/impl/ItemServiceImpl.java
+++ mall-manager/mall-manager-service/src/main/java/com/zhzhgang/mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.zhzhgang.mall.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zhzhgang.mall.common.pojo.ResponseResult;
 import com.zhzhgang.mall.mapper.MallItemMapper;
 import com.zhzhgang.mall.pojo.MallItem;
+import com.zhzhgang.mall.pojo.MallItemExample;
 import com.zhzhgang.mall.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 商品管理 Service
@@ -22,4 +28,18 @@ public class ItemServiceImpl implements ItemService {
         return mallItemMapper.selectByPrimaryKey(itemId);
     }
 
+    @Override
+    public ResponseResult getItemList(int page, int rows) {
+        PageHelper.startPage(page, rows);
+
+        MallItemExample example = new MallItemExample();
+        List<MallItem> list = mallItemMapper.selectByExample(example);
+
+        PageInfo<MallItem> pageInfo = new PageInfo<MallItem>(list);
+
+        ResponseResult responseResult = new ResponseResult();
+        responseResult.setRows(list);
+        responseResult.setTotal(pageInfo.getTotal());
+        return responseResult;
+    }
 }
