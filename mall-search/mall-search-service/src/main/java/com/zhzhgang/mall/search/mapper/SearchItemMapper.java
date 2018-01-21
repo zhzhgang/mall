673addaa0a7bc8b15c7c07bd68c685c46922b1e5
张忠钢 <zhang_zhonggang@163.com>
2018-01-21 22:09:43 +0800

    导入商品数据到索引库 - dao 实现

---
+++ mall-search/mall-search-service/src/main/java/com/zhzhgang/mall/search/mapper/SearchItemMapper.java
@@ -0,0 +1,10 @@
+package com.zhzhgang.mall.search.mapper;
+
+import com.zhzhgang.mall.common.pojo.SearchItem;
+
+import java.util.List;
+
+public interface SearchItemMapper {
+
+    List<SearchItem> getItemList();
+}
