e391937a270154d3721549e76c5cd5d50d95d08c
张忠钢 <zhang_zhonggang@163.com>
2018-03-15 21:12:14 +0800

    用户登录 Controller

--- mall-sso-web/src/main/java/com/zhzhgang/mall/sso/controller/UserController.java
+++ mall-sso-web/src/main/java/com/zhzhgang/mall/sso/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.zhzhgang.mall.sso.controller;
 
 import com.zhzhgang.mall.common.pojo.MallResult;
+import com.zhzhgang.mall.common.utils.CookieUtils;
 import com.zhzhgang.mall.pojo.MallUser;
 import com.zhzhgang.mall.sso.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 用户处理 Controller
@@ -19,6 +24,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Value("${TOKEN_KEY}")
+    private String tokenKey;
+
     @RequestMapping("/user/check/{param}/{type}")
     public MallResult checkUserData(@PathVariable String param, @PathVariable int type) {
         MallResult result = userService.checkData(param, type);
@@ -28,6 +36,15 @@ public class UserController {
     @RequestMapping(value = "/user/register", method = RequestMethod.POST)
     public MallResult register(@RequestBody MallUser user) {
         MallResult result = userService.register(user);
+        return result;
+    }
+
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    public MallResult login(@RequestBody MallUser user, HttpServletResponse response, HttpServletRequest request) {
+        MallResult result = userService.login(user.getUsername(), user.getPassword());
+
+        // 把 token 写入 cookie
+        CookieUtils.setCookie(request, response, tokenKey, result.getData().toString());
         return result;
     }
 }
